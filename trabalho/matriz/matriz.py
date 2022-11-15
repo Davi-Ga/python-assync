9bb9944598b65cf986c5a8643ffc9de77711f6f4
Davi-Ga <davigaldinoky@gmail.com>
2022-11-15 00:38:18 -0300

    criação da matriz implementada

---
+++ trabalho/matriz/matriz.py
@@ -0,0 +1,23 @@
+import random
+from worker_matriz import worker
+
+def gerar_matriz():
+    matriz = []
+    for i in range(1):
+        linha = []
+        for j in range(10):
+            linha.append(random.randint(1, 100))
+        matriz.append(linha)
+    return matriz
+
+def checar_matriz(matriz):
+    for linha in matriz:
+        for elemento in linha:
+            if elemento == 0:
+                print('Multiplicação falhou!')
+    return print('Multiplicação bem sucedida!')
+
+if __name__ == '__main__':
+    matriz = gerar_matriz()
+    for linha in matriz:
+        print(linha)
