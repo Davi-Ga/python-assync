cb26ac19df85788479ebdf14ee6276c176ab37e1
Zander404 <alexandrearthurpg@gmail.com>
2022-11-19 19:19:06 -0300

    tentando o hash multi-threading

---
+++ trabalho/hash/worker.py
@@ -0,0 +1,65 @@
+import hashlib
+
+
+dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+
+#Ideia: criar um vetor de worker que receba - woker = []
+# worker.push[<nome da função worker/theading>[lenght(dict)/MAXWORKERS]]
+
+
+worker = []
+worker.append(dict[15])
+worker.append(dict[0])
+worker.append(dict[20])
+ 
+
+hash = 'ed968e840d10d2d313a870bc131a4e2c311d7ad09bdf32b3418147221f51a6e2' #aaaaa
+
+
+def crack(i,j,k,l,str):
+    global dict
+    str.append(dict[i])
+    str.append(dict[j])
+    str.append(dict[k])
+    str.append(dict[l])
+
+    hashB = hashlib.sha256(''.join(str).encode())
+    resultB = hashB.hexdigest()
+    print(resultB)
+
+    if(hash == str):
+        print(resultB)
+
+
+    else:
+        print(resultB)
+        l+=1
+        if(l==25):
+            k+=1
+        if(k==25):
+            j+=1
+        if(j==25):
+            i=+1
+
+        if (i == 25):
+            exit
+        
+        str.remove(dict[i])
+        str.remove(dict[j])
+        str.remove(dict[k])
+        str.remove(dict[l])
+        
+        crack(i,j,k,l,str)
+
+
+i = 0 #Worker 1 
+j = 0 #Worker 2
+k = 0 #Worker 3
+l = 0 #Worker 4
+
+
+str = []
+str.append('a')
+crack(i,j,k,l,str)
