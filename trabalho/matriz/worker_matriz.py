94a88b98ac003e12973878c6f0c34a41e09ae8e5
Davi-Ga <davigaldinoky@gmail.com>
2022-11-18 23:01:44 -0300

    nome da variável alterada

--- trabalho/matriz/worker_matriz.py
+++ trabalho/matriz/worker_matriz.py
@@ -1,7 +1,7 @@
 import threading
 import random
 
-def worker(matrizX,matrizY,matriz_resultante,i,j):
+def worker_multiplica_matriz(matrizX,matrizY,matriz_resultante,i,j):
     worker_name = threading.current_thread().name
 
     matriz_resultante[i][j] += matrizX[i][j] * matrizY[i][j]
