0168f2c5b1ec93f8ea25fa30ea7c3c68c47ea9e9
Zander404 <alexandrearthurpg@gmail.com>
2022-11-19 21:45:25 -0300

    repair hash

--- trabalho/hash/worker.py
+++ trabalho/hash/worker.py
@@ -15,7 +15,7 @@ worker.append(dict[0])
 worker.append(dict[20])
  
 
-hash = 'ed968e840d10d2d313a870bc131a4e2c311d7ad09bdf32b3418147221f51a6e2' #aaaaa
+hash = 'ed968e840d10d2d313a870bc131a4e2c311d7ad09bdf32b3418147221f51a6e5' #aaaaa
 
 
 def crack(i,j,k,l):
@@ -23,31 +23,48 @@ def crack(i,j,k,l):
     str = []
     str.append('a')
 
-    str.append(dict[i])
-    str.append(dict[j])
-    str.append(dict[k])
-    str.append(dict[l])
+    for i in range (26):
+        for j in range(26):
+            for k in range (26):
+                for l in range(26):
+                        str.append(dict[i])
+                        str.append(dict[j])
+                        str.append(dict[k])
+                        str.append(dict[l])
 
-    hashB = hashlib.sha256(''.join(str).encode())
-    resultB = hashB.hexdigest()
+                        hashB = hashlib.sha256(''.join(str).encode())
+                        resultB = hashB.hexdigest()
+                        
+                        if(hash == resultB):
+                            print("Achei")
+                            print(f"A palavra decodificada é: {''.join(str)}")
+                        print(str)
+                        str.pop()
+                        str.pop()
+                        str.pop()
+                        str.pop()
+                        
+    
 
 
-    if(hash == resultB):
-       print("Achei")
-       print(f"A palavra decodificada é: {''.join(str)}")
+                        
+        # l+=1
+        # print(l)
+        # if(l==25):
+        #     l=0
+        #     k+=1
+        # if(k==25):
+        #     k=0
+        #     j+=1
 
-    else:
-        l+=1
-        print(l)
-        if(l==25):
-            l=0
-            k+=1
-        if(k==25):
-            k=0
-            j+=1
-            print('k:',k)
+        # if(j==26):
+        #     j=0
+        #     i+=1
+        # if (i==26):
+        #     return
+        # crack(i,j,k,l)
 
-        crack(i,j,k,l)
+
 
 
 i = 0 #Worker 1 
