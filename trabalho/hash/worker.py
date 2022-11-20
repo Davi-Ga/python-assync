034067cdaa42057fe38f50724070cfccb06abe0f
Zander404 <alexandrearthurpg@gmail.com>
2022-11-19 21:56:35 -0300

    fazer threading disso

--- trabalho/hash/worker.py
+++ trabalho/hash/worker.py
@@ -10,19 +10,14 @@ dict = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
 
 
 worker = []
-worker.append(dict[15])
-worker.append(dict[0])
-worker.append(dict[20])
+
  
 
 hash = 'ed968e840d10d2d313a870bc131a4e2c311d7ad09bdf32b3418147221f51a6e5' #aaaaa
 
 
-def crack(i,j,k,l):
+def crack(str,i,j,k,l):
     global dict
-    str = []
-    str.append('a')
-
     for i in range (26):
         for j in range(26):
             for k in range (26):
@@ -74,5 +69,5 @@ l = 0 #Worker 4
 
 
 str = []
-str.append('a')
-crack(i,j,k,l)
+worker.append(dict[15]) #Passar isso para os workers 
+crack(worker,i,j,k,l)
