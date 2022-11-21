ace5eb772ee110dc58ed1edac1c2784cd11ad580
Davi-Ga <davigaldinoky@gmail.com>
2022-11-21 08:36:05 -0300

    prints retirados

--- trabalho/matriz/worker.py
+++ trabalho/matriz/worker.py
@@ -3,12 +3,12 @@ import random
 
 def worker_multiplica_matriz(matrizX,matrizY,matriz_resultante,i,j):
     worker_name = threading.current_thread().name
-    print('Worker: %s' % worker_name)
+
     matriz_resultante[i][j] += matrizX[i][j] * matrizY[i][j]
     
 def worker_gerar_matriz(matriz,tam_matriz):
     worker_name = threading.current_thread().name
-    print('Worker: %s' % worker_name)
+
     for i in range(tam_matriz):
         linha = []
         for j in range(tam_matriz):
