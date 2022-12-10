f9a36f226bf23739904132e932031f58f902c995
Davi-Ga <davigaldinoky@gmail.com>
2022-12-10 16:18:04 -0300

    page

--- integrador/scripts/request.py
+++ integrador/scripts/request.py
@@ -2,10 +2,10 @@ from pyodide.http import XMLHttpRequest,pyfetch
 from pyodide.ffi import JsException
 import pandas as pd
 
-async def highscore():
+async def highscore(page):
     try:
         response = await pyfetch(
-            url=f"https://api.tibiadata.com/v3/highscores/Antica/swordfighting/paladins/1",
+            url=f"https://api.tibiadata.com/v3/highscores/Antica/swordfighting/paladins/{page}",
             method="GET",
             header={"Content-Type": "application/json"},
         )
@@ -18,8 +18,8 @@ async def highscore():
     except JsException as e:
         return e
 
-async def get_highscore():
-    high=await highscore()
+async def get_highscore(page):
+    high=await highscore(page)
     if high is None:
         return None
     else:
@@ -28,3 +28,4 @@ async def get_highscore():
 def dict_to_dataframe(highscore):
     df=pd.DataFrame.from_dict(highscore)
     return df
+
